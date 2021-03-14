c1729cc4239d279e92a58c14ce899c436dd6cb2d
Karol Moroz <k.j.moroz@gmail.com>
2021-03-14 16:39:07 +0800

    Sticky footer, kind of working blog archive

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,24 +25,14 @@ const Layout = ({ children }) => {
     >
       <div className="layout" lang={htmlLang}>
         <Header />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0 1.0875rem 1.45rem`,
-          }}
-        >
-          <main>{children}</main>
-          <footer
-            style={{
-              marginTop: `2rem`,
-            }}
-          >
+        <main>{children}</main>
+        <footer className="footer">
+          <p>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.com">Gatsby</a>
-          </footer>
-        </div>
+          </p>
+        </footer>
       </div>
     </IntlProvider>
   )
